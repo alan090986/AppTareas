3bb8c8ecf7edd46794b1dab2abb962045269134a
alan090986 <49327642+alan090986@users.noreply.github.com>
2026-06-26 11:40:14 -0600

    Bajar limite de conexiones para Clever Cloud

--- db.js
+++ db.js
@@ -9,5 +9,9 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: parseInt(process.env.DB_PORT)|| 3306, 
+  waitForConnections: true,
+
+  connectionLimit: 3, 
+  queueLimit: 0
 });
