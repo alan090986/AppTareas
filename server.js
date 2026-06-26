bef5c0ff9a4c15c5c32560ced36068604ab0e411
alan090986 <49327642+alan090986@users.noreply.github.com>
2026-06-25 19:44:39 -0600

    Corregir bloqueo de CORS para Vercel

--- server.js
+++ server.js
@@ -12,21 +12,15 @@ const app = express();
 // 2. Definir orígenes permitidos para el Frontend
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://apptareas-oxyp.onrender.com'
 ];
 
 // 3. Configurar CORS (Debe ir ARRIBA de las rutas)
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Bloqueado por políticas de CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true
+  origin: '*', // Permite que cualquier dominio (como tu Vercel) consuma tu API
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // 4. Interceptor manual para solicitudes Preflight (OPTIONS)
