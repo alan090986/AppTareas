c4d3ba7184ca0c43e70ca674b3590b7d79c430df
alan090986 <49327642+alan090986@users.noreply.github.com>
2026-06-26 16:19:40 -0600

    Forzar lectura nativa de variables en Docker

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-// db.js
-import mysql from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 import dotenv from 'dotenv';
 
+// Ejecuta dotenv  en local
 dotenv.config();
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT)|| 3306, 
+//  inyecta las credenciales directo.
+export const pool = createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'app_tareas',
+  port: parseInt(process.env.DB_PORT) || 3307,
   waitForConnections: true,
-
-  connectionLimit: 3, 
+  connectionLimit: 3,
   queueLimit: 0
 });
